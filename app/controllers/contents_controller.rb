7d7a96fa95d28c00ffe3c076d2c2b77f4625097a
kj-tatsukawa <kuj.tatsukawa@gmail.com>
2022-01-14 22:24:50 +0900

    contents_controller.rb 修正

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -40,7 +40,7 @@ class ContentsController < ApplicationController
   end
 
   def update
-    @page_detail = Page.find_by(params[:id])
+    @page_detail = Page.find_by(id: params[:id])
     if @page_detail.update(page_params)
       redirect_to("/#{@page_detail.content_type}/#{@page_detail.id}")
     else
