56d0aa0ab555baea7681e77f0fa54eed0312c547
kj-tatsukawa <kuj.tatsukawa@gmail.com>
2022-01-24 00:41:14 +0900

    記述変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,25 @@
 Rails.application.routes.draw do
-  # ====== home ======
-  get '/', to: 'home#how_to_page'
-  get 'top' => 'home#top'
+  # ========== home ==========
+  root 'home#how_to_page'
+
+  get 'top', to: 'home#top'
   get 'new_category', to: 'home#new_category'
   get 'home/delete/administrator', to: 'home#delete_btn'
 
   post 'create_category', to: 'home#create_category'
+
   delete 'destroy', to: 'home#destroy'
 
 
   # ====== contents ======
-  get ':menu_url/index' => 'contents#index'
+  get ':menu_url/index', to: 'contents#index'
   get 'new', to: 'contents#new'
-  get ':content_type/:id' => 'contents#show'
-  get ':content_type/:id/edit' => 'contents#edit'
+  get ':content_type/:id', to: 'contents#show'
+  get ':content_type/:id/edit', to: 'contents#edit'
 
   post 'create_post', to: 'contents#create_post'
+
   patch ':content_type/:id/update', to: 'contents#update'
-  # post ':content_type/:id/update', to: 'contents#update'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
